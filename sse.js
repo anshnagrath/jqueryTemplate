abb65b7c18989c5f6280cabf3bfa85efbaeb99d3
anshnagrath <anshnagrath448@gmail.com>
2019-06-10 08:20:56 +0530

    bassic functionality ready

--- sse.js
+++ sse.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       //    var parentInputLength =document.
       if(lastQuestionInputLength>0){
       elementCounter++;
-      $(".firstRow").append('<div class="content"><div  class="" style="margin-bottom: 20px !important"><div class="input-field"><p style="margin-left:-26px;padding-top:9px;">'+elementCounter+'</p> <input id="email" name="question" type="text" placeholder="Option"  required><div id="e2"></div> </div><div style="float: right !important;" class="input-field"  > <select id="select"style="display:block;"> <option value="1"  disabled selected>Choose Question type</option><option value="radio">Radio</option> <option value="Checkbox">Checkbox</option><option value="Mutiselect">Muti select</option>     </select></div>    <div class="input-field" style="width:55% !important ;margin-top: 0px" id="options"> <input placeholder="CoorectAnswer" name="answer'+elementCounter+'" type="text"  required> <input class="firstOption" name="option'+elementCounter+'" id="ops" onkeypress="EnterEvent(event)" type="text" placeholder="Option" required>    <i style="float:right;bottom:53px;left:10px;position:relative" onClick="EnterEvent('+fromIcon+')" class="material-icons addElement">add_circle</i></a> <i style="float:right;margin-top:-53px;left:10px" class="material-icons"  onClick="removeOptions()">remove_circle</i></a></div></div>')
+      $(".firstRow").append('<div class="content"><div  class="" style="margin-bottom: 20px !important"><div class="input-field"><p style="margin-left:-26px;padding-top:9px;">'+elementCounter+'</p> <input id="email" name="question" type="text" placeholder="Option"  required><div id="e2"></div> </div><div style="float: right !important;" class="input-field"  > <select id="select"style="display:block;"> <option value="1"  disabled selected>Choose Question type</option><option value="radio">Radio</option> <option value="Checkbox">Checkbox</option><option value="Mutiselect">Muti select</option>     </select></div>    <div class="input-field" style="width:55% !important ;margin-top: 0px" id="options"> <input placeholder="CoorectAnswer" name="answer'+elementCounter+'" type="text"  required> <div><input class="firstOption" name="option'+elementCounter+'" id="ops" onkeypress="EnterEvent(event)" type="text" placeholder="Option" required>    <i style="float:right;bottom:53px;left:10px;position:relative" onclick="EnterEvent('+fromIcon+')" class="material-icons addElement">add_circle</i></a> <i style="float:right;position:relative;bottom:52px;left:10px" class="material-icons removeElement"  onclick="RemoveOptions(event)">remove_circle</i></div></div></div>')
      }else{
       M.toast({html: 'Please fill the previous details and then proceed'})
      }
@@ -25,13 +25,14 @@ $(document).ready(function() {
     //to add a option on enter
 
     $("#ops").last().on('keypress',function(e){
-        var parentId = document.activeElement.parentElement.parentElement.getElementsByTagName('p')[0].innerText;
+      
+      var parentId =  $(document.activeElement).closest('.content').find('p')[0].innerText;
          
        setTimeout(()=>{ 
          console.log( document.activeElement.value.length)
         if(e.which == 13 && document.activeElement.value.length>0 && document.getElementById('email').value.length>0) {
           console.log(document.activeElement.value,'eessszeffff') 
-          $("#ops").append('<input  name="option'+parentId+'" type="text" on id="ops" placeholder="Option" required>    <i style="float:right;bottom:53px;left:10px;position:relative" class="material-icons addElement"  onClick="EnterEvent('+fromIcon+')">add_circle</i></a>  <i style="float:right;margin-top:-53px;left:10px"  onClick="removeOptions()" class="material-icons">remove_circle</i></a> ')
+          $("#ops").append('<div><input  name="option'+parentId+'" type="text" on id="ops" placeholder="Option" required>    <i style="float:right;bottom:53px;left:10px;position:relative" class="material-icons addElement"  onclick="EnterEvent('+fromIcon+')">add_circle</i></a> <i style="float:right;position:relative;bottom:52px;left:10px"  onclick="RemoveOptions(event)" class="material-icons removeElement">remove_circle</i></div>')
          }
          else if(document.activeElement.value.length ==0 && e.which == 13 &&  document.getElementById('email').value && document.getElementById('email').value.length==0 ||document.getElementById('email').value &&  document.getElementById('email').value.length==0 && e.which == 13 ){
          M.toast({html: 'Please fill the previous details and then proceed'})
@@ -41,6 +42,10 @@ $(document).ready(function() {
     
           },100)      
         });
+
+
+
+
 
       //to get all the value from a particular form 
     
@@ -108,9 +113,11 @@ $(document).ready(function() {
 
           })
       });
+
       $('.addElement').click(function(e) {
-       var parentId = $(this).parent().parent().find('p')[0].innerText
-        $(this).parent().append("<input class='firstOption' placeholder='Option' name='option"+parentId+"' type='text' onkeypress='EnterEvent(event)' id='ops' required>    <i style='float:right;bottom:53px;left:10px;position:relative' class='material-icons addElement' onClick = 'EnterEvent("+fromIcon+")'>add_circle</i></a> <i style='float:right;margin-top:-53px;left:10px'class='material-icons' onClick='removeOptions()'>remove_circle</i></a>")    
+        console.log("heeee")
+       var parentId = $(this).closest('.content').find('p')[0].innerText
+        $(this).parent().append("<div><input class='firstOption' placeholder='Option' name='option"+parentId+"' type='text' onkeypress='EnterEvent(event)' id='ops' required>    <i style='float:right;bottom:53px;left:10px;position:relative' class='material-icons addElement' onclick = 'EnterEvent("+fromIcon+")'>add_circle</i></a> <i  style='float:right;position:relative;bottom:52px;left:10px' class='material-icons removeElement' onclick='RemoveOptions()'>remove_circle</i></div>")    
        });
      
    
@@ -121,24 +128,33 @@ $(document).ready(function() {
      if(e.keyCode ==13) {
     var parentInputLength =document.activeElement.parentElement.parentElement.getElementsByTagName('input')[0].value.length;
     if(e.keyCode == 13 && document.activeElement.value.length>0 && parentInputLength>0) {
-     var parentId = document.activeElement.parentElement.parentElement.getElementsByTagName('p')[0].innerText;
+     var parentId =  $(document.activeElement).closest('.content').find('p')[0].innerText;
+    
        console.log( document.activeElement)
-        $(document.activeElement.parentElement).append('<input class="firstOption" placeholder="Option" name="option'+parentId+'" type="text" onkeypress="EnterEvent(event)" id="ops" required>    <i style="float:right;bottom:53px;left:10px;position:relative" onClick="EnterEvent('+fromIcon+')" class="material-icons addElement">add_circle</i></a> <i style="float:right;margin-top:-53px;left:10px"  onClick="removeOptions()" class="material-icons">remove_circle</i></a>')
+        $(document.activeElement.parentElement).append('<div><input class="firstOption" placeholder="Option" name="option'+parentId+'" type="text" onkeypress="EnterEvent(event)" id="ops" required>    <i style="float:right;bottom:53px;left:10px;position:relative" onclick="EnterEvent('+fromIcon+')" class="material-icons addElement">add_circle</i></a><i style="float:right;position:relative;bottom:52px;left:10px"  onclick="RemoveOptions(event)" class="material-icons removeElement">remove_circle</i></div>')
      }else if(e.keyCode == 13 && document.activeElement.value.length==0){
       M.toast({html: 'Please fill the previous details and then proceed'})
      } 
     }else if(e=='fromIcon'){
       $('.addElement').click(function(event){
-    // console.log($(this).parent())
-    var parentId =  $(this).parent().parent().find('p')[0].innerText;
-   $(this).parent().append("<input class='firstOption' placeholder='Option' name='option"+parentId+"' type='text' onkeypress='EnterEvent(event)' id='ops' required>    <i style='float:right;bottom:53px;left:10px;position:relative' class='material-icons addElement' onClick = 'EnterEvent("+fromIcon+")'>add_circle</i></a> <i style='float:right;margin-top:-53px;left:10px' onClick='removeOptions()' class='material-icons'>remove_circle</i></a>")    
+     
+    var parentId = $(this).closest('.content').find('p')[0].innerText;
+   $(this).parent().append("<div><input class='firstOption' placeholder='Option' name='option"+parentId+"' type='text' onkeypress='EnterEvent(event)' id='ops' required>    <i style='float:right;bottom:53px;left:10px;position:relative' class='material-icons addElement' onclick = 'EnterEvent("+fromIcon+")'>add_circle</i></a> <i style='float:right;position:relative;bottom:52px;left:10px' onclick='RemoveOptions(event)' class='material-icons removeElement'>remove_circle</i></div>")    
     event.stopImmediatePropagation();
       })
      
      
      }
  }
- function removeOptions(){
-
-
- }
+function RemoveOptions(){
+  $('.removeElement').click(function(event){
+    console.log("testttttsssttttt",$(this).parent().length)
+  // if($(this).parent().parent().length>1){
+   $(this).parent().remove();
+    event.stopImmediatePropagation(); 
+  
+    // }else{
+    //   M.toast({html: 'Cannot remove a mandatory option'})
+    // }
+  })
+}
